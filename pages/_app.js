736f34d5f5f13c5853d57f07776c6f21a3d2b908
xinghengwang <xing@moesif.com>
2022-06-28 14:32:44 -0700

    added more example.

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,12 @@ function MyApp({ Component, pageProps }) {
       });
       // this below assigns moesif object to the global window space, so you can trigger window.moesif in other places without having to init it again.
       window.moesif = moesif;
+
+      // then anywhere you can start to track user actions using the window.moesif object
+      window.moesif.track('viewed-landing-page', { foo: 'hello' });
+
+      // or when you have user info like when user loges in:
+      // window.moesif.identifyUser('theirUserId', { email: 'blah@nowhere.com', first_name: 'John' });
     });
   }, []);
   return <Component {...pageProps} />
