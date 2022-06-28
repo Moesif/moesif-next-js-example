0905f6f064c6d8e527441fa8e975703a7c27ee08
xinghengwang <xing@moesif.com>
2022-06-28 16:17:13 -0700

    comemnt it out

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,15 @@ import { useEffect } from 'react';
 // if you are using server side rendering.
 // you can only import moesif-browser-js on the client side.
 // approach 1 for ensuring only importing moesif-browser-js in browser:
-if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
-  import('moesif-browser-js').then((moesif) => {
-    moesif.init({
-      applicationId: 'You Application Id'
-    });
-    // this below assigns moesif object to the global window space, so you can trigger window.moesif in other places without having to init it again.
-    window.moesif = moesif;
-  });
-}
+// if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+//   import('moesif-browser-js').then((moesif) => {
+//     moesif.init({
+//       applicationId: 'You Application Id'
+//     });
+//     // this below assigns moesif object to the global window space, so you can trigger window.moesif in other places without having to init it again.
+//     window.moesif = moesif;
+//   });
+// }
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
